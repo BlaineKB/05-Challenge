97f84919d949dc4acf964a5a0c3eb3ad089f53f7
BKB <blainekb94@gmail.com>
2022-09-28 16:46:10 -0500

    added classes to timeblock containers and added save buttons with icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-//
+now = moment();
+console.log(now);
+
+test = moment().format('kk');
+console.log(test);
+
+test2 = moment().format('MMMM Do YYYY, h:mm a');
+console.log(test2);
+
 
 // function time( ) {
 
